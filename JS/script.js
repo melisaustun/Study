98eac23c3305037ad23b619fcc4453741ea45368
Melisa <meliisaustun@gmail.com>
2021-12-07 15:25:40 +0300

    .

---
+++ JS/script.js
@@ -0,0 +1,106 @@
+//console.log("External");
+//let selam="Hello";
+
+//console.log(selam);
+
+//let selam ="Merhaba";
+
+//console.log(selam);
+
+//var deger = "Hellooo";
+//console.log(deger);
+//var deger = "Merhaba";
+//console.log(deger);
+
+//function fonn(){
+//    var deger ="text";
+//}
+
+//fonn();
+//console.log(deger);
+
+
+//const text = "Javascript";
+//text = "Hello";
+
+var ders= "Ders";
+var ders= 'Ders'; //aynılar
+
+var sayi = 1;
+var ondalik = 1.15;
+
+var bool = true; 
+var bool = false;
+
+//undefined
+
+var a;
+console.log("deger", typeof(a));
+
+
+var b = null;
+
+console.log("type", typeof(b));
+
+
+let id = Symbol();
+
+
+//Reference
+
+var lesson = {
+    name:'Web',
+    number: 1
+}
+
+console.log("Object", lesson);
+console.log("Object", lesson.name);
+
+//Array
+
+var dizi =[1, 2, 3];
+
+console.log("dizi", typeof(dizi));
+
+console.log("item", dizi[1]);
+
+dizi[3]= 4;
+console.log(dizi);
+
+console.log(dizi.length);
+
+// Function
+
+//function hello(text){
+//   console.log("Function", text);
+//}
+//var text = "Text";
+//hello(text); //çağırdık
+
+var num = "20c"; //=true;
+var ynum = Number(num);
+
+console.log(ynum);
+
+var str =1;
+var ss =String(str);
+console.log(ss);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
